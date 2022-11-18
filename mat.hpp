204079b926fd717454cff1ad0982e369f7c67896
0sani <jackahansen18@gmail.com>
2022-11-18 10:05:30 -0800

    Fixed matrix multiplication

--- mat.hpp
+++ mat.hpp
@@ -6,42 +6,30 @@ template<typename T, size_t rows, size_t cols>
 class Matrix {
 private:
     constexpr static size_t _size = rows * cols;
-	T _contents[_size]{};
+	T _contents[_size]{0};
 public:
-    Matrix() = default;
+	Matrix() = default;
 
-    //Diagonal Matrix Constructor
-    template <std::enable_if_t<rows == cols, bool> = true>
-    Matrix(const std::array<T, rows>& arr) { 
-        for (size_t i = 0; i < rows; ++i)
-            _contents[(i * rows) + i] = arr[i];
-    }
+	//Diagonal Matrix Constructor
+	Matrix(const std::array<T, std::min(rows, cols)>& arr) { 
+		for (size_t i = 0; i < std::min(rows, cols); ++i)
+			_contents[(i * cols) + i] = arr[i];
+	}
 
-	Matrix(const std::array<T, _size>& data) {
-		_contents = new T[_size];
+	explicit Matrix(const std::array<T, _size>& data) {
 		for (size_t i = 0; i < _size; i++) {
 			_contents[i] = data[i];
 		}
 	}
-	
-	// Diagonal Matrix Constructor
-	Matrix(const std::array<T, std::min(rows, cols)>& data) {
-		_contents = new T[_size];
-		for (size_t i = 0; i < std::min(rows, cols); i++) {
-			_contents[i*(cols+1)]= data[i];
-		}
-	}
-
-	
 
 	T& operator[](size_t index) {
 		assert(index < _size);
 		return _contents[index];
 	}
 
-	T& operator()(size_t x, size_t y) {
-		assert(x < rows && y < cols);
-		return _contents[x*rows + y];
+	T& operator()(size_t row, size_t col) {
+		assert(row < rows && col < cols);
+		return _contents[row*cols + col];
 	}
 
 	Matrix& operator*=(T scalar) {
@@ -86,14 +74,14 @@ public:
 	}
 
 	template<size_t C>
-	Matrix operator*(Matrix<T, cols, C> other) {
+	Matrix<T, rows, C> operator*(Matrix<T, cols, C> other) {
 		Matrix<T, rows, C> res;
 		for (size_t i = 0; i < rows; i++) {
 			for (size_t j = 0; j < C; j++) {
 				T sum = 0;
-				for (size_t k = 0; k < _rows; k++){
+				for (size_t k = 0; k < rows; k++){
 
-					sum += _contents[i*_rows+k] * other(k, j);
+					sum += _contents[i*cols+k] * other(k, j);
 				}
 				res(i,j) = sum;
 			}
