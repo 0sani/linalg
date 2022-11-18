958dc5d70d9dbf2f0f3fa954e427d016520a6ea4
0sani <jackahansen18@gmail.com>
2022-11-17 18:43:51 -0800

    Added arithmetic operators: Addition, subtraction, scalar multiplication/division, matrix multiplication

--- mat.hpp
+++ mat.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 
 
 template<typename T, size_t rows, size_t cols>
@@ -23,10 +24,64 @@ public:
 		return _contents[row*_rows + col];
 	}
 
-	int operator*=(int scalar) {
+
+	Matrix& operator*=(T scalar) {
 		for (size_t i = 0; i < _size; i++)
 			_contents[i] *= scalar;
-		return scalar;
+		return *this;
+	}
+
+
+	Matrix& operator+=(Matrix<T, rows, cols> other) {
+		for (size_t i = 0; i < _size; i++)
+			_contents[i] += other[i];
+		return *this;
+	}
+
+	Matrix& operator-=(Matrix<T, rows, cols> other) {
+		for (size_t i = 0; i < _size; i++)
+			_contents[i] -= other[i];
+		return *this;
+	}
+
+	Matrix& operator/=(T scalar) {
+		for (size_t i = 0; i < _size; i++)
+			_contents[i] /= scalar;
+		return *this;
+	}
+
+	Matrix operator+(Matrix<T, rows, cols> other) {
+		Matrix<T, rows, cols> res;
+		for (size_t i = 0; i < _size; i++) {
+			res[i] = _contents[i] + other[i];
+		}
+		return res;
+	}
+
+	Matrix operator-(Matrix<T, rows, cols> other) {
+		Matrix<T, rows, cols> res;
+		for (size_t i = 0; i < _size; i++) {
+			res[i] = _contents[i] - other[i];
+		}
+		return res;
+	}
+
+	template<size_t R, size_t C>
+	Matrix operator*(Matrix<T, R, C> other) {
+		assert(cols == R);
+		Matrix<T, rows, C> res;
+		for (size_t i = 0; i < rows; i++) {
+			for (size_t j = 0; j < C; j++) {
+				T sum = 0;
+				for (size_t k = 0; k < rows; k++){
+
+					sum += _contents[i*rows+k] * other(k, j);
+				}
+				res(i,j) = sum;
+			}
+			
+		}
+		return res;
 	}
 
 };
