32e2a9e2183695c8ee652647ec102adb76fe3fd7
0sani <jackahansen18@gmail.com>
2022-11-18 22:43:00 -0800

    Added reduced row echelon form (and the basis of a basis class) <-- haha aren't i funny i am losing my sanity that took way too long

---
+++ basis.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cassert>
+#include <array>
+#include "mat.hpp"
+
+template<typename T, size_t rows, size_t cols>
+class Basis {
+private:
+    std::vector<Matrix<T, rows, cols>> elems;
+public:
+
+
+bool linearly_independent() {
+    
+}
+
+};
--- mat.hpp
+++ mat.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <array>
+#include <cmath>
 
 template<typename T, size_t rows, size_t cols>
 class Matrix {
@@ -92,10 +93,100 @@ public:
 	friend std::ostream &operator<<(std::ostream &output, Matrix<T, rows, cols> mat) {
 	for (size_t i = 0; i < rows; i++) {
 		for (size_t j = 0; j < cols; j++) {
-			output << mat(i,j) << " ";
+			output << mat(i,j) << "\t";
 		}
 		output << std::endl;
 	}
 	return output;
 	}
+
+	Matrix row_interchange(size_t a, size_t b) {
+		assert(a < rows && b < rows);
+		if (a == b) return *this;
+		T temp;
+		for (size_t i = 0; i < cols; i++) {
+			temp = _contents[a*cols + i];
+			_contents[a*cols + i] = _contents[b*cols + i];
+			_contents[b*cols + i] = temp;
+		}
+		return *this;
+	}
+
+	Matrix row_scaling(size_t row, T scalar) {
+		assert(row < rows);
+		for (size_t i = 0; i < cols; ++i) {
+			_contents[row*cols + i] *= scalar;
+		}
+		return *this;
+	}
+
+	Matrix row_replacement(size_t a, size_t b, T scalar) {
+		assert(a < rows && b < rows && a != b);
+		for (size_t i = 0; i < cols; ++i) {
+			_contents[a*cols+i] -= _contents[b*cols+i] * scalar;
+		}
+		return *this;
+	}
+
+
+	Matrix RREF() {
+		Matrix<T, rows, cols> res = *this;
+		int p_row = 0;
+		int p_col = 0;
+
+		// Forward phase
+		while (p_row < rows && p_col < cols) {			
+			// find k-th pivot
+			// argmax
+			size_t i_max = 0;
+			T highest = 0;
+			for (size_t i = p_row; i < rows; ++i) {
+				if (std::abs(res(i, p_col)) >= highest) {
+					highest = std::abs(res(i, p_col));
+					i_max = i;
+				}
+			}
+
+			if (res(i_max, p_col) == 0) {
+				++p_col;
+			} else {
+				res.row_interchange(p_row, i_max);
+
+				for (size_t i = p_row + 1; i < rows; ++i) {
+					T f = res(i, p_col) / res(p_row, p_col);
+					res.row_replacement(i, p_row, f);
+				}
+				++p_row;
+				++p_col;
+			}
+		}
+
+		// Backward phase
+		p_row = rows - 1;
+		p_col = cols - 1;
+
+		while (p_row >= 0 && p_col >= 0) {
+			// find pivot col
+			for (size_t i = 0; i < cols; ++i) {
+				if (res(p_row, i)) {
+					p_col = i;
+					break;
+				}
+			}
+
+			// scale pivot row so leading num is 1
+			T f = (res(p_row, p_col)) ? 1 / res(p_row, p_col) : 0;
+
+			// create zeros above pivot col
+			res.row_scaling(p_row, f);
+			for (int i = p_row-1; i >= 0; --i) {
+				res.row_replacement(i, p_row, res(i, p_col));
+			}
+			p_row--;
+			p_col--;
+		}
+
+		return res;
+	}
+
 };
